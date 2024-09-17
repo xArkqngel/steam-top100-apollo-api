f29ca754d5e76973971bd8d2b036d152f466d2e0
xArkqngel <arkqngel343@gmail.com>
2024-09-17 10:28:12 -0500

    port and destination changed

--- dev.js
+++ dev.js
@@ -2,4 +2,4 @@ import app from "./src/index.js";
 import consola from "consola";
 import dotenv from "dotenv";
 dotenv.config();
-app.listen(3001, () => consola.info("Server started"));
+app.listen(3000, () => consola.info("Server started"));
