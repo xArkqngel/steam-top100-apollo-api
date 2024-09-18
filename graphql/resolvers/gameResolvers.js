75fdda926d3f11587285bd564ce045ed396677f6
xArkqngel <arkqngel343@gmail.com>
2024-09-18 11:02:02 -0500

    Preprocess tags before resolving

--- graphql/resolvers/gameResolvers.js
+++ graphql/resolvers/gameResolvers.js
@@ -1,48 +1,40 @@
-import data from "../../public/games.json" assert { type: "json" };
+import rawData from "../../public/games.json" assert { type: "json" };
 
 const transformTags = (tags) => {
   return Object.entries(tags).map(([key, value]) => ({
     name: key,
-    count: parseInt(value),
+    count: value,
   }));
 };
+
+const data = Object.entries(rawData).reduce((acc, [id, game]) => {
+  if (game.tags) {
+    game.tags = transformTags(game.tags);
+  }
+  acc[id] = game;
+  return acc;
+}, {});
 
 const gameResolvers = {
   Query: {
     game: (_, { id }) => {
       const game = data[id];
-      if (game && game.tags) {
-        game.tags = transformTags(game.tags);
-      }
       return { id, ...game };
     },
     games: () => {
       return Object.entries(data)
         .filter(([_, game]) => !game.is_dlc)
-        .map(([id, game]) => {
-          if (game.tags) {
-            game.tags = transformTags(game.tags);
-          }
-          return { id, ...game };
-        });
+        .map(([id, game]) => ({ id, ...game }));
     },
     dlcs: () => {
       return Object.entries(data)
         .filter(([_, game]) => game.is_dlc)
-        .map(([id, game]) => {
-          if (game.tags) {
-            game.tags = transformTags(game.tags);
-          }
-          return { id, ...game };
-        });
+        .map(([id, game]) => ({ id, ...game }));
     },
     game_dlcs: (_, { id }) => {
       const game_dlc_list = data[id].dlc;
       return game_dlc_list.map((dlc_id) => {
         const dlc = data[dlc_id];
-        if (dlc.tags) {
-          dlc.tags = transformTags(dlc.tags);
-        }
         return { id: dlc_id, ...dlc };
       });
     },
